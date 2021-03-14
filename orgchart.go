fec367a529ae88e5f56415886babb6daeff4690f
gilmae <avocadia@fastmail.fm>
2021-03-14 15:40:01 +1100

    Implement GetSubordinates
    Includes fixes from a linting and changed visibility for usersInRole

--- orgchart.go
+++ orgchart.go
@@ -1,52 +1,80 @@
 package orgchart
 
+import "fmt"
+
 type Role struct {
-	Id int
-	Name string
+	Id     int
+	Name   string
 	Parent int
 }
 
 type User struct {
-	Id int
+	Id   int
 	Name string
 	Role int
 }
 
 type Organisation struct {
-	Roles map[int]Role
+	Roles    map[int]Role
 	roleTree map[int][]int
 
 	Users map[int]User
 
-	UsersInRole map[int][]User
-	
+	usersInRole map[int][]User
 }
 
-func NewOrganisation() *Organisation{
+func NewOrganisation() *Organisation {
 	return &Organisation{
-		Roles: make(map[int]Role),
-		roleTree: make(map[int][]int),
-		Users: make(map[int]User),
-		UsersInRole: make(map[int][]User),
+		Roles:       make(map[int]Role),
+		roleTree:    make(map[int][]int),
+		Users:       make(map[int]User),
+		usersInRole: make(map[int][]User),
 	}
+}
+
+func (o *Organisation) GetSubordinates(userId int) ([]User, error) {
+	subordinates := []User{}
+	user, ok := o.Users[userId]
+	
+	if !ok {
+		return subordinates, fmt.Errorf("User not found")
+	}
+
+	subRoles, ok := o.roleTree[user.Role]
+	if !ok {
+		return subordinates, nil
+	}
+
+	for _, roleId := range subRoles {
+		if users, ok := o.usersInRole[roleId]; ok {
+			for _, u := range users {
+				subordinates = append(subordinates, u)
+				subSub, err := o.GetSubordinates(u.Id)
+				if err == nil {
+					subordinates = append(subordinates, subSub...)
+				}
+			}
+		}
+	}
+	return subordinates, nil
 }
 
 func (o *Organisation) SetRoles(roles []Role) {
 	o.Roles = make(map[int]Role)
 	o.roleTree = make(map[int][]int)
 
-	o.UsersInRole = make(map[int][]User)
+	o.usersInRole = make(map[int][]User)
 	for _, role := range roles {
 		o.Roles[role.Id] = role
-		
+
 		// Forgo referential integrity check for now
-		if _,ok := o.roleTree[role.Parent]; !ok {
-			o.roleTree[role.Parent] = []int {role.Id}
+		if _, ok := o.roleTree[role.Parent]; !ok {
+			o.roleTree[role.Parent] = []int{role.Id}
 		} else {
 			o.roleTree[role.Parent] = append(o.roleTree[role.Parent], role.Id)
 		}
 
-		o.UsersInRole[role.Id] = make([]User, 0)
+		o.usersInRole[role.Id] = make([]User, 0)
 	}
 	o.mapUsersToRoles()
 }
@@ -61,8 +89,8 @@ func (o *Organisation) SetUsers(users []User) {
 
 func (o *Organisation) mapUsersToRoles() {
 	for _, user := range o.Users {
-		if users, ok := o.UsersInRole[user.Role]; ok {
-			o.UsersInRole[user.Role] = append(users, user)
+		if users, ok := o.usersInRole[user.Role]; ok {
+			o.usersInRole[user.Role] = append(users, user)
 		}
 	}
 }
--- orgchart_test.go
+++ orgchart_test.go
@@ -5,104 +5,9 @@ import (
 	"testing"
 )
 
-type UserInRoleTest struct {
-	roleId int
-	users  []User
-}
-
-func TestSetRoles(t *testing.T) {
-
-	tests := [][]Role{
-		[]Role{Role{Id: 1, Name: "foo", Parent: 0}},
-		[]Role{
-			Role{Id: 1, Name: "bar", Parent: 0},
-			Role{Id: 2, Name: "baz", Parent: 1},
-		},
-	}
-
-	for _, roles := range tests {
-		o := NewOrganisation()
-		o.SetRoles(roles)
-
-		if len(o.Roles) != len(roles) {
-			t.Errorf("Incorrect number of roles in organisation, expected %d, got %d", len(roles), len(o.Roles))
-		}
-
-		if len(o.UsersInRole) != len(roles) {
-			t.Errorf("Incorrect number of roles in user to role mapping in organisation, expected %d, got %d", 1, len(o.Roles))
-		}
-
-		for _, role := range roles {
-			r, ok := o.Roles[role.Id]
-			if !ok {
-				t.Fatalf("Could not resolve Role# %d in organisation roles", role.Id)
-			}
-
-			if r != role {
-				t.Errorf("Role does not match, expected %+v, got %+v", roles[0], r)
-			}
-
-			users, ok := o.UsersInRole[role.Id]
-			if !ok {
-				t.Fatalf("Could not resolve Role# %d in user to role mapping", role.Id)
-			}
-			if len(users) != 0 {
-				t.Errorf("Users in role not empty for Role#%d", role.Id)
-			}
-		}
-	}
-}
-
-func TestSetRolesWithExistingUsers(t *testing.T) {
-	userData := []User{
-		User{Id: 1, Name: "doh", Role: 1},
-		User{Id: 2, Name: "ray", Role: 2},
-		User{Id: 3, Name: "me", Role: 2},
-	}
-	roles := []Role{
-		Role{Id: 1, Name: "foo", Parent: 0},
-		Role{Id: 2, Name: "bar", Parent: 1},
-		Role{Id: 3, Name: "baz", Parent: 1},
-	}
-
-	tests := []UserInRoleTest{
-		{
-			roleId: 1,
-			users:  userData[0:1],
-		},
-		{
-			roleId: 2,
-			users:  userData[1:3],
-		},
-		 {
-		 	roleId: 3,
-		 	users:  []User{},
-		 },
-	}
-
-	o := NewOrganisation()
-	o.SetUsers(userData)
-	o.SetRoles(roles)
-
-	for _, tt := range tests {
-		users, ok := o.UsersInRole[tt.roleId]
-		if !ok {
-			t.Errorf("Could not resolve users in Role #%d", tt.roleId)
-		}
-
-		if len(users) != len(tt.users) {
-			t.Errorf("Incorrect number of users in Role #%d, expected %d, got %d",
-				tt.roleId,
-				len(tt.users),
-				len(users))
-		}
-
-		for _,u := range tt.users {
-			if !containsUser(users, u) {
-				t.Errorf("User not found in role, got %+v", users)
-			}
-		}
-	}
+type userInRoleTestCase struct {
+	roleId        int
+	expectedUsers []User
 }
 
 func TestRoleTree(t *testing.T) {
@@ -144,6 +49,89 @@ func TestRoleTree(t *testing.T) {
 		}
 	}
 }
+func TestSetRoles(t *testing.T) {
+
+	tests := [][]Role{
+		[]Role{Role{Id: 1, Name: "foo", Parent: 0}},
+		[]Role{
+			Role{Id: 1, Name: "bar", Parent: 0},
+			Role{Id: 2, Name: "baz", Parent: 1},
+		},
+	}
+
+	for _, roles := range tests {
+		o := NewOrganisation()
+		o.SetRoles(roles)
+
+		if len(o.Roles) != len(roles) {
+			t.Errorf("Incorrect number of roles in organisation, expected %d, got %d", len(roles), len(o.Roles))
+		}
+
+		if len(o.usersInRole) != len(roles) {
+			t.Errorf("Incorrect number of roles in user to role mapping in organisation, expected %d, got %d", 1, len(o.Roles))
+		}
+
+		for _, role := range roles {
+			r, ok := o.Roles[role.Id]
+			if !ok {
+				t.Fatalf("Could not resolve Role# %d in organisation roles", role.Id)
+			}
+
+			if r != role {
+				t.Errorf("Role does not match, expected %+v, got %+v", roles[0], r)
+			}
+
+			expectedUsers, ok := o.usersInRole[role.Id]
+			if !ok {
+				t.Fatalf("Could not resolve Role# %d in user to role mapping", role.Id)
+			}
+			if len(expectedUsers) != 0 {
+				t.Errorf("Users in role not empty for Role#%d", role.Id)
+			}
+		}
+	}
+}
+
+func TestSetRolesWithExistingUsers(t *testing.T) {
+	userData := []User{
+		User{Id: 1, Name: "doh", Role: 1},
+		User{Id: 2, Name: "ray", Role: 2},
+		User{Id: 3, Name: "me", Role: 2},
+	}
+	roles := []Role{
+		Role{Id: 1, Name: "foo", Parent: 0},
+		Role{Id: 2, Name: "bar", Parent: 1},
+		Role{Id: 3, Name: "baz", Parent: 1},
+	}
+
+	tests := []userInRoleTestCase{
+		{
+			roleId:        1,
+			expectedUsers: userData[0:1],
+		},
+		{
+			roleId:        2,
+			expectedUsers: userData[1:3],
+		},
+		{
+			roleId:        3,
+			expectedUsers: []User{},
+		},
+	}
+
+	o := NewOrganisation()
+	o.SetUsers(userData)
+	o.SetRoles(roles)
+
+	for _, tt := range tests {
+		users, ok := o.usersInRole[tt.roleId]
+		if !ok {
+			t.Errorf("Could not resolve users in Role #%d", tt.roleId)
+		}
+
+		testExpectedUsers(t, users, tt.expectedUsers)
+	}
+}
 
 func TestSetUsers(t *testing.T) {
 	tests := [][]User{
@@ -154,22 +142,22 @@ func TestSetUsers(t *testing.T) {
 		},
 	}
 
-	for _, users := range tests {
+	for _, expectedUsers := range tests {
 		o := NewOrganisation()
-		o.SetUsers(users)
+		o.SetUsers(expectedUsers)
 
-		if len(o.Users) != len(users) {
-			t.Errorf("Incorrect number of users in organisation, expected %d, got %d", len(users), len(o.Users))
+		if len(o.Users) != len(expectedUsers) {
+			t.Errorf("Incorrect number of expectedUsers in organisation, expected %d, got %d", len(expectedUsers), len(o.Users))
 		}
 
-		for _, user := range users {
+		for _, user := range expectedUsers {
 			u, ok := o.Users[user.Id]
 			if !ok {
-				t.Fatalf("Could not resolve User# %d in organisation users", user.Id)
+				t.Fatalf("Could not resolve User# %d in organisation expectedUsers", user.Id)
 			}
 
 			if u != user {
-				t.Errorf("User does not match, expected %+v, got %+v", users[0], u)
+				t.Errorf("User does not match, expected %+v, got %+v", expectedUsers[0], u)
 			}
 		}
 	}
@@ -187,19 +175,19 @@ func TestSetUsersWithExistingRoles(t *testing.T) {
 		Role{Id: 3, Name: "baz", Parent: 1},
 	}
 
-	tests := []UserInRoleTest{
+	tests := []userInRoleTestCase{
 		{
-			roleId: 1,
-			users:  userData[0:1],
+			roleId:        1,
+			expectedUsers: userData[0:1],
 		},
 		{
-			roleId: 2,
-			users:  userData[1:3],
+			roleId:        2,
+			expectedUsers: userData[1:3],
 		},
-		 {
-		 	roleId: 3,
-		 	users:  []User{},
-		 },
+		{
+			roleId:        3,
+			expectedUsers: []User{},
+		},
 	}
 
 	o := NewOrganisation()
@@ -207,31 +195,72 @@ func TestSetUsersWithExistingRoles(t *testing.T) {
 	o.SetUsers(userData)
 
 	for _, tt := range tests {
-		users, ok := o.UsersInRole[tt.roleId]
+		users, ok := o.usersInRole[tt.roleId]
 		if !ok {
-			t.Errorf("Could not resolve users in Role #%d", tt.roleId)
+			t.Errorf("Could not resolve expectedUsers in Role #%d", tt.roleId)
 		}
 
-		if len(users) != len(tt.users) {
-			t.Errorf("Incorrect number of users in Role #%d, expected %d, got %d",
-				tt.roleId,
-				len(tt.users),
-				len(users))
-		}
-
-		for _,u := range tt.users {
-			if !containsUser(users, u) {
-				t.Errorf("User not found in role, got %+v", users)
-			}
-		}
+		testExpectedUsers(t, users, tt.expectedUsers)
 	}
 }
 
-func containsUser(users []User, user User) bool {
-	for _,u := range users {
-		if u==user {
+func TestGetSubordinateUsers(t *testing.T) {
+	userData := []User{
+		User{Id: 1, Name: "doh", Role: 1},
+		User{Id: 2, Name: "ray", Role: 4},
+		User{Id: 3, Name: "me", Role: 3},
+		User{Id: 4, Name: "fah", Role: 2},
+		User{Id: 5, Name: "sol", Role: 5},
+	}
+	roles := []Role{
+		Role{Id: 1, Name: "foo", Parent: 0},
+		Role{Id: 2, Name: "bar", Parent: 1},
+		Role{Id: 3, Name: "baz", Parent: 2},
+		Role{Id: 4, Name: "qux", Parent: 3},
+		Role{Id: 5, Name: "quux", Parent: 3},
+	}
+
+	tests := []struct {
+		userId        int
+		expectedUsers []User
+	}{
+		{3, []User{userData[1], userData[4]}},
+		{1, userData[1:]},
+	}
+
+	o := NewOrganisation()
+	o.SetRoles(roles)
+	o.SetUsers(userData)
+
+	for _, tt := range tests {
+		users, err := o.GetSubordinates(tt.userId)
+
+		if err != nil {
+			t.Errorf("Received error: %s", err)
+		}
+		testExpectedUsers(t, users, tt.expectedUsers)
+	}
+}
+
+func containsUser(expectedUsers []User, user User) bool {
+	for _, u := range expectedUsers {
+		if u == user {
 			return true
 		}
 	}
 	return false
+}
+
+func testExpectedUsers(t *testing.T, users []User, expectedUsers []User) {
+	if len(users) != len(expectedUsers) {
+		t.Errorf("Incorrect number of users, expected %d, got %d",
+			len(expectedUsers),
+			len(users))
+	}
+
+	for _, u := range expectedUsers {
+		if !containsUser(users, u) {
+			t.Errorf("User not found, %+v", u)
+		}
+	}
 }
